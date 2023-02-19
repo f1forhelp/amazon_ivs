9fff0383aee5978476f91c02e7220015bcb030c2
Utkarsh Sharma <rickysharma1111@gmail.com>
2023-02-19 18:06:24 +0530

    SampleUiCompleted

--- ios/Classes/AmazonIvsPlugin.swift
+++ ios/Classes/AmazonIvsPlugin.swift
@@ -1,14 +1,73 @@
 import Flutter
 import UIKit
+import AVKit
+import AmazonIVSPlayer
 
 public class AmazonIvsPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "amazon_ivs", binaryMessenger: registrar.messenger())
-    let instance = AmazonIvsPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "amazon_ivs", binaryMessenger: registrar.messenger())
+        let instance = AmazonIvsPlugin()
+            
+        let factory = FLNativeViewFactory(messenger: registrar.messenger())
+        
+        registrar.register(factory, withId: "test_p")
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    
+    }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+}
+
+
+class FLNativeViewFactory: NSObject, FlutterPlatformViewFactory {
+    private var messenger: FlutterBinaryMessenger
+
+    init(messenger: FlutterBinaryMessenger) {
+        self.messenger = messenger
+        super.init()
+    }
+
+    func create(
+        withFrame frame: CGRect,
+        viewIdentifier viewId: Int64,
+        arguments args: Any?
+    ) -> FlutterPlatformView {
+        return FLNativeView(
+            frame: frame,
+            viewIdentifier: viewId,
+            arguments: args,
+            binaryMessenger: messenger)
+    }
+}
+
+class FLNativeView: NSObject, FlutterPlatformView {
+    private var _view: UIView
+
+    init(
+        frame: CGRect,
+        viewIdentifier viewId: Int64,
+        arguments args: Any?,
+        binaryMessenger messenger: FlutterBinaryMessenger?
+    ) {
+        _view = UIView()
+        super.init()
+        // iOS views can be created here
+        createNativeView(view: _view)
+    }
+
+    func view() -> UIView {
+        return _view
+    }
+
+    func createNativeView(view _view: UIView){
+        _view.backgroundColor = UIColor.blue.withAlphaComponent(0)
+        let nativeLabel = UILabel()
+        nativeLabel.text = "Native text from iOS"
+        nativeLabel.textColor = UIColor.black
+        nativeLabel.textAlignment = .center
+        nativeLabel.frame = CGRect(x: 0, y: 0, width: 180, height: 48.0)
+        _view.addSubview(nativeLabel)
+    }
 }
--- lib/amazon_ivs.dart
+++ lib/amazon_ivs.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/src/widgets/framework.dart';
 
 import 'amazon_ivs_platform_interface.dart';
 
 class AmazonIvs {
   Future<String?> getPlatformVersion() {
     return AmazonIvsPlatform.instance.getPlatformVersion();
+  }
+}
+
+class TestPlayer extends StatelessWidget {
+  const TestPlayer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // This is used in the platform side to register the view.
+    const String viewType = 'test_p';
+    // Pass parameters to the platform side.
+    final Map<String, dynamic> creationParams = <String, dynamic>{};
+
+    return UiKitView(
+      viewType: viewType,
+      layoutDirection: TextDirection.ltr,
+      creationParams: creationParams,
+      creationParamsCodec: const StandardMessageCodec(),
+    );
   }
 }
